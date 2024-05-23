f94be83212fe9f1f7dae65afe95a861352ddc021
박준후 <joonhoo0123@gmail.com>
2024-05-23 15:15:37 +0900

    init project

---
+++ HikeApp/HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by 박준후 on 5/23/24.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
