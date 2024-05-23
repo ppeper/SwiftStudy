2e921de078008b51a5429a7582dfc2f9ecef33d0
박준후 <joonhoo0123@gmail.com>
2024-05-23 16:41:31 +0900

    add Extension color, ZStack CardView

---
+++ HikeApp/HikeApp/Component/CardView.swift
@@ -0,0 +1,41 @@
+//
+//  CardView.swift
+//  HikeApp
+//
+//  Created by 박준후 on 5/23/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        // MARK: - CARD
+        
+        ZStack {
+            CustomBackgroundView()
+            
+            ZStack {
+                Circle()
+                    .fill(
+                        LinearGradient(
+                            colors: [
+                                Color.customIndigoMedium,
+                                Color.customSalmonLight
+                            ],
+                            startPoint: .topLeading,
+                            endPoint: .bottomTrailing
+                        )
+                    )
+                    .frame(width: 256, height: 256)
+                Image("image-1")
+                    .resizable()
+                .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
+    }
+}
+
+#Preview {
+    CardView()
+}
