d7a67d6311eb55f5987e35597f32b8334865166c
박준후 <joonhoo0123@gmail.com>
2024-05-28 16:05:47 +0900

    init project

---
+++ PinchApp/PinchApp/PinchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchAppApp.swift
+//  PinchApp
+//
+//  Created by 박준후 on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
