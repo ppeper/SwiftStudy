025471752d400d5341882cae16c83c1ea640b9ba
박준후 <joonhoo0123@gmail.com>
2024-05-28 21:06:09 +0900

    init project

---
+++ Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 박준후 on 5/28/24.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
