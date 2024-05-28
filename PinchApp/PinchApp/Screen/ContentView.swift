65362c6e90665ce731988edfe1be7670fdf52880
박준후 <joonhoo0123@gmail.com>
2024-05-28 16:50:06 +0900

    add info panel

--- PinchApp/PinchApp/Screen/ContentView.swift
+++ PinchApp/PinchApp/Screen/ContentView.swift
@@ -12,14 +12,23 @@ struct ContentView: View {
     
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
     // MARK: - FUNCTION
+    
+    func resetImageState() {
+        return withAnimation(.spring) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     
     // MARK: - CONTENT
     
     var body: some View {
         NavigationView {
             ZStack {
+                Color.clear
                 // MARK: - PAGE IMAGE
                 Image("magazine-front-cover")
                     .resizable()
@@ -28,6 +37,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
                 // MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
@@ -36,11 +46,23 @@ struct ContentView: View {
                                 imageScale = 5
                             }
                         } else {
-                            withAnimation(.spring) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                    // MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 0.5)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded { _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            }
+                    )
             } //: ZSTACK
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -49,7 +71,13 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
-            
+            // MARK: - INFO PANEL
+            .overlay(
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30),
+                alignment: .top
+            )
         } //: NAVIGATION
         .navigationViewStyle(.stack)
     }
