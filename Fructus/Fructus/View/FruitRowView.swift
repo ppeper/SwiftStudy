97164d738f5daaa7e403838b50059fc9e823954b
박준후 <joonhoo0123@gmail.com>
2024-05-28 22:55:09 +0900

    add FruitRowView, NavigationView

---
+++ Fructus/Fructus/View/FruitRowView.swift
@@ -0,0 +1,45 @@
+//
+//  FruitRowView.swift
+//  Fructus
+//
+//  Created by 박준후 on 5/28/24.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    // MARK: - PROPERTIES
+    
+    var fruit: Fruit
+
+    // MARK: - BODY
+    
+    var body: some View {
+        HStack {
+            Image(fruit.image)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: .center)
+                .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.3), radius: 3, x: 2, y: 2)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom)
+                )
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundColor(Color.secondary)
+            }
+        } //: HSTACK
+    }
+}
+
+#Preview(traits: .sizeThatFitsLayout) {
+    FruitRowView(fruit: fruitsData[0])
+        .padding()
+}
