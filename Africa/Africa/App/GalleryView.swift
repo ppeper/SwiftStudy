4fefc012523356cb83ab82b73da72bff7559a87e
박준후 <joonhoo0123@gmail.com>
2024-05-29 17:46:16 +0900

    add Flexible GridLayout Gallery

--- Africa/Africa/App/GalleryView.swift
+++ Africa/Africa/App/GalleryView.swift
@@ -8,9 +8,72 @@
 import SwiftUI
 
 struct GalleryView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var selectedAnimal: String = "lion"
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    
+    // SIMPLE GRID DEFINITION
+    // let gridLayout: [GridItem] = [
+    //     GridItem(.flexible()),
+    //     GridItem(.flexible()),
+    //     GridItem(.flexible()),
+    // ]
+    
+    // EFFICIENT GRID DEFINITION
+//    let gridLayout: [GridItem] = Array(repeating: GridItem(.flexible()), count: 3)
+    
+    // DYNAMIC GRID LAYOUT
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColunm: Double = 3.0
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: Int(gridColunm))
+    }
+    
+    // MARK: - BODY
+    
     var body: some View {
         ScrollView(.vertical, showsIndicators: false) {
-            Text("Gallery")
+            VStack(alignment: .center, spacing: 30) {
+                // MARK: - IMAGE
+                
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                // MARK: - SLIDER
+                Slider(value: $gridColunm, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: gridColunm) {
+                        gridSwitch()
+                    }
+                
+                // MARK: - GRID
+                
+                LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.image)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.image
+                            }
+                    } //: LOOP
+                } //: GRID
+                .animation(.easeIn)
+                .onAppear {
+                    gridSwitch()
+                }
+            } //: VSTACK
+            .padding(.horizontal, 10)
+            .padding(.vertical, 50)
         } //: SCROLL
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())
