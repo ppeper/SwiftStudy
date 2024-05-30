907e3a4ab8431bad8625332f06431659a4c5787a
박준후 <joonhoo0123@gmail.com>
2024-05-30 12:43:09 +0900

    init project

---
+++ Touchdown/Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by 박준후 on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
