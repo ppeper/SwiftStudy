2569778b39cf01a4c98a561864367bd7518fd4c9
박준후 <joonhoo0123@gmail.com>
2024-06-04 13:00:13 +0900

    init project

---
+++ Avocados/Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by 박준후 on 6/4/24.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
