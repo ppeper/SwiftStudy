9310afcffaa7782a919d0bec38d449780e592fa0
박준후 <joonhoo0123@gmail.com>
2024-06-04 15:09:36 +0900

    add AvocadosView

--- Avocados/Avocados/View/AvocadosView.swift
+++ Avocados/Avocados/View/AvocadosView.swift
@@ -8,8 +8,55 @@
 import SwiftUI
 
 struct AvocadosView: View {
+    // MARK: - PROPERTIES
+    
+    @State private var pulsateAnimation: Bool = false
+    
+    // MARK: - BODY
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Image("avocado")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 240, height: 240, alignment: .center)
+                .shadow(color: Color("ColorBlackTransparentDark"), radius: 12, x: 0, y: 0)
+                .scaleEffect(pulsateAnimation ? 1 : 0.9)
+                .opacity(pulsateAnimation ? 1 : 0.9)
+                .animation(.easeInOut(duration: 1.5).repeatForever(autoreverses: true), value: pulsateAnimation)
+            
+            VStack {
+                Text("Avocados".uppercased())
+                    .font(.system(size: 42, weight: .bold, design: .serif))
+                    .foregroundColor(Color.white)
+                    .padding()
+                .shadow(color: Color("ColorBlackTransparentDark"), radius:4, x: 0, y: 0)
+                
+                Text("""
+                Creamy, green, and full of nutrients!
+                Avocado is a powerhouse ingredient at any meal. Enjoy these handpicked avocado recipes for breakfast, lunch, dinner & more!
+                """)
+                .lineLimit(nil)
+                .font(.system(.headline, design: .serif))
+                .foregroundColor(Color("ColorGreenLight"))
+                .multilineTextAlignment(.center)
+                .lineSpacing(8)
+                .frame(maxWidth: 640, minHeight: 120)
+                
+            } //: VSTACK
+            Spacer()
+            
+        } //: VSTACK
+        .background(
+            Image("background")
+                .resizable()
+                .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+        )
+        .onAppear {
+            self.pulsateAnimation.toggle()
+        }
+        .edgesIgnoringSafeArea(.top)
     }
 }
 
