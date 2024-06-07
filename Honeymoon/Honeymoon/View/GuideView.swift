e22fa78761b592c84c2a797b64817667d176390b
박준후 <joonhoo0123@gmail.com>
2024-06-07 17:38:06 +0900

    add GuideView

---
+++ Honeymoon/Honeymoon/View/GuideView.swift
@@ -0,0 +1,75 @@
+//
+//  GuideView.swift
+//  Honeymoon
+//
+//  Created by 박준후 on 6/7/24.
+//
+
+import SwiftUI
+
+struct GuideView: View {
+    var body: some View {
+        ScrollView(.vertical, showsIndicators: false) {
+            VStack(alignment: .center, spacing: 20) {
+                HeaderComponent()
+                
+                Spacer(minLength: 10)
+                
+                Text("Get Started!")
+                    .fontWeight(.black)
+                    .font(.largeTitle)
+                    .foregroundColor(Color.pink)
+                
+                Text("Discover and pick the perfect destiniation for your romantic Honeymoon!")
+                    .lineLimit(nil)
+                    .multilineTextAlignment(.center)
+                
+                Spacer(minLength: 10)
+                
+                VStack(alignment: .leading, spacing: 25) {
+                    GuideComponent(
+                        title: "Like",
+                        subtitle: "Swipe right",
+                        description: "Do you like ths destination? Touch the screen and swipe right. It will be saved to the favorites.",
+                        icon: "heart.circle"
+                    )
+                    
+                    GuideComponent(
+                        title: "Dismiss",
+                        subtitle: "Swipe left",
+                        description: "Would you rather skip this place? Touch the screen and swipe left. Tou will no longer see it.",
+                        icon: "xmark.circle"
+                    )
+                    
+                    GuideComponent(
+                        title: "Book",
+                        subtitle: "Tap the button",
+                        description: "Out selection of honeymoon resorts is perfect setting for you embark your life together.",
+                        icon: "checkmark.square"
+                    )
+                }
+                Spacer(minLength: 10)
+                
+                Button {
+                    print("Button Tap")
+                } label: {
+                    Text("Continue".uppercased())
+                        .font(.headline)
+                        .padding()
+                        .frame(minWidth: 0, maxWidth: .infinity)
+                        .background(Capsule().fill(Color.pink))
+                        .foregroundColor(Color.white)
+                }
+                
+            }
+            .frame(minWidth: 0, maxWidth: .infinity)
+            .padding(.top, 15)
+            .padding(.bottom, 25)
+            .padding(.horizontal, 25)
+        } //: SCROLL
+    }
+}
+
+#Preview {
+    GuideView()
+}
