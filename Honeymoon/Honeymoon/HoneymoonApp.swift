899fef029ec2409f8664c9b518a0e3ac3423eb76
박준후 <joonhoo0123@gmail.com>
2024-06-07 13:27:39 +0900

    init project

---
+++ Honeymoon/Honeymoon/HoneymoonApp.swift
@@ -0,0 +1,17 @@
+//
+//  HoneymoonApp.swift
+//  Honeymoon
+//
+//  Created by 박준후 on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct HoneymoonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
